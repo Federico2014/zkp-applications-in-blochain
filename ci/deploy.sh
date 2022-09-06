0f0a1d8b3d8c2fd4ec9c6e8b2248118f0534a3f0
federico <federico.zhen@tron.network>
2022-09-06 11:05:18 +0800

    add GH_TOKEN

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,7 +6,7 @@ git init
 git config user.name "Rust-Fuzz GitHub Action"
 git config user.email "no-reply@fake.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/rust-fuzz/book.git"
+git remote add upstream "https://$GH_TOKEN@github.com/Federico2014/zkp-applications-in-blochain.git"
 git fetch upstream
 git reset upstream/gh-pages
 
